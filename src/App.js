842f4e2c4fa4db4fb4afff5c429cc587ca2b387f
saravana3252 <sara18ec118@gmail.com>
2024-12-09 22:01:12 +0530

    superhero image gallery

---
+++ src/App.js
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from 'react';
+import Header from './Header';
+import Superhero from './superheros';
+
+function App() {
+
+
+  let [data,setData]=useState([])
+
+useEffect(()=>{
+  fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json").then((response)=>response.json()).then((data)=>{
+    setData(data)
+    console.log(data)
+  })
+},[])
+
+
+  return (
+    <div>
+      <Header></Header>
+      <div className="w-full  bg-black p-5 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
+
+
+
+        {
+          data.map((superhero)=>{
+             return <Superhero superheros={superhero}></Superhero>
+          })
+        }
+    
+     
+
+     </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div className="w-full bg-black flex flex-col items-center p-3">
+            <h1 id="logo" className="font-bold text-6xl text-white lg:text-8xl text-glow">SUPERHERO</h1>
+            <p className="text-white text-3xl pt-2 font-medium lg:text-4xl">IMAGE GALLERY</p>
+        </div>
+    )
+}
+
+export default Header;
