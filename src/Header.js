bbe33be6b7a7861190ab9a2decfe69e1212cae8f
saravana3252 <sara18ec118@gmail.com>
2024-12-10 18:32:12 +0530

    font size updated -tab view

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 function Header(props){
     return(
         <div className="w-full bg-black flex flex-col items-center p-2">
-            <h1 id="logo" className="font-bold text-6xl text-white lg:text-8xl text-glow">SUPERHERO</h1>
-            <p className="text-white text-3xl pt-2 font-medium lg:text-4xl">IMAGE GALLERY</p>
+            <h1 id="logo" className="font-bold text-6xl text-white md:text-8xl">SUPERHERO</h1>
+            <p className="text-white text-3xl pt-2 font-medium md:text-4xl">IMAGE GALLERY</p>
             <div className="flex justify-center  w-full p-4 ">
             <input id="inpVal" type="search" placeholder="search superhero" className="w-[90%] lg:w-[80%] p-2 outline-none rounded-lg focus:ring-4 focus:ring-red-400 focus:bg-blue-100 " onChange={(e)=>{
               let filterValue = props.superheros.filter((data)=>{
