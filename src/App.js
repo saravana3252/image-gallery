430d60500ebb598b804cb47f5aac4fe147c3c26f
saravana3252 <sara18ec118@gmail.com>
2024-12-10 09:50:35 +0530

    search function added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ function App() {
 
   let [data,setData]=useState([])
 
+  let [filterData,setFilterData]=useState([])
+
+
+function superheroFun(value){
+  setData(value)
+}
+
 useEffect(()=>{
   fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json").then((response)=>response.json()).then((data)=>{
     setData(data)
+    setFilterData(data)
     console.log(data)
   })
 },[])
@@ -18,11 +26,8 @@ useEffect(()=>{
 
   return (
     <div>
-      <Header></Header>
+      <Header superheros={filterData} superheroFun={superheroFun}></Header>
       <div className="w-full  bg-black p-5 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
-
-
-
         {
           data.map((superhero)=>{
              return <Superhero superheros={superhero}></Superhero>
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,21 @@
-function Header(){
+function Header(props){
     return(
-        <div className="w-full bg-black flex flex-col items-center p-3">
+        <div className="w-full bg-black flex flex-col items-center p-2">
             <h1 id="logo" className="font-bold text-6xl text-white lg:text-8xl text-glow">SUPERHERO</h1>
             <p className="text-white text-3xl pt-2 font-medium lg:text-4xl">IMAGE GALLERY</p>
+            <div className="flex justify-center  w-full p-4 ">
+            <input id="inpVal" type="search" placeholder="search superhero" className="w-[90%] lg:w-[80%] p-2 outline-none rounded-lg focus:ring-4 focus:ring-red-400 focus:bg-blue-100 " onChange={(e)=>{
+              let filterValue = props.superheros.filter((data)=>{
+                return data.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1;
+              })
+              if(e.target.value !== ""){
+              props.superheroFun(filterValue)
+              }
+              else{
+                props.superheroFun(props.superheros)
+              }
+            }}></input>
+            </div>
         </div>
     )
 }
