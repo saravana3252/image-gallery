de54ed1774d88fcbbe48421532c1c6049bfbe6c9
saravana3252 <sara18ec118@gmail.com>
2024-12-10 10:04:56 +0530

    search updated

--- src/App.js
+++ src/App.js
@@ -27,17 +27,18 @@ useEffect(()=>{
   return (
     <div>
       <Header superheros={filterData} superheroFun={superheroFun}></Header>
-      <div className="w-full  bg-black p-5 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
+
+
+      { data.length > 0 ? ( 
+      <div className="w-full  bg-black py-3 px-5 grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4">
         {
           data.map((superhero)=>{
              return <Superhero superheros={superhero}></Superhero>
           })
         }
-    
-     
-
      </div>
-     
+      ):(<div className='w-full h-[500px] md:h-[850px] lg:h-[400px] bg-black flex justify-center items-center text-4xl font-bold text-white'>superhero not found</div>)
+      }
     </div>
   );
 }
