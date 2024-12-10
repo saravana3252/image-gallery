b5940229e1ce5a5ea7ca60cf83fb9eb157e15151
saravana3252 <sara18ec118@gmail.com>
2024-12-10 19:02:36 +0530

    opacity updated1 -superhero.js

--- src/superheros.js
+++ src/superheros.js
@@ -2,7 +2,7 @@ function Superhero(props){
     return (
         <div >
             <div className="relative overflow-hidden group">
-                <div id="overlay" className="w-full h-full flex flex-col gap-1 lg:gap-2 justify-center  items-center lg:justify-center bg-black bg-opacity-0 absolute top-0 transition-all duration-500 cursor-pointer group-hover:bg-opacity-70">
+                <div id="overlay" className="w-full h-full flex flex-col gap-1 lg:gap-2 justify-center  items-center lg:justify-center bg-black bg-opacity-0 absolute top-0 transition-all duration-500 cursor-pointer group-hover:bg-opacity-60">
                     <h1 className="opacity-0 transition-all duration-500 text-white text-xl md:text-3xl font-bold group-hover:opacity-100">POWERSTATS</h1>
                     <p className="opacity-0 transition-all duration-500 text-white text-md md:text-2xl group-hover:opacity-100">intelligence : {props.superheros.powerstats.intelligence}</p>
                     <p className="opacity-0 transition-all duration-500 text-white text-md md:text-2xl  group-hover:opacity-100">strength : {props.superheros.powerstats.strength}</p>
