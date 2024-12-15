9b6e9bfb02804052077cf8c79564a9682df6e28f
saravana3252 <sara18ec118@gmail.com>
2024-12-15 19:23:54 +0530

    header.js -input focus updated

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ function Header(props){
             <p className="text-white text-4xl pt-1 font-medium md:text-6xl">IMAGE GALLERY</p>
             {/* <p className="italic text-yellow-100 pt-2">An Epic Gallery of Superheroes and Villains</p> */}
             <div className="flex justify-center  w-full p-3 md:p-4">
-            <input id="inpVal" type="search" placeholder="Find Your Favourite Hero or Villain" className="w-[90%] lg:w-[80%] p-2 outline-none rounded-lg focus:ring-4 focus:ring-red-400 focus:bg-blue-100 " onChange={(e)=>{
+            <input id="inpVal" type="search" placeholder="Find Your Favourite Hero or Villain" className="w-[90%] lg:w-[80%] p-2 outline-none rounded-lg focus:ring-4 focus:ring-red-700 focus:bg-red-200 " onChange={(e)=>{
               let filterValue = props.superheros.filter((data)=>{
                 return data.name.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1;
               })
