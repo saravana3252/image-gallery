30c9b2a76cdca88f1a5dae474ab4fc5122b31d7a
saravana3252 <sara18ec118@gmail.com>
2024-12-15 19:21:32 +0530

    header.js -font adjusted

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 function Header(props){
     return(
         <div className="w-full bg-black flex flex-col pt-2 items-center">
-            <h1 id="logo" className="font-bold text-6xl text-white md:text-8xl lg:text-9xl">EPIC</h1>
-            <p className="text-white text-3xl pt-1 font-medium md:text-6xl">IMAGE GALLERY</p>
+            <h1 id="logo" className="font-bold text-8xl text-white md:text-8xl lg:text-9xl">EPIC</h1>
+            <p className="text-white text-4xl pt-1 font-medium md:text-6xl">IMAGE GALLERY</p>
             {/* <p className="italic text-yellow-100 pt-2">An Epic Gallery of Superheroes and Villains</p> */}
             <div className="flex justify-center  w-full p-3 md:p-4">
             <input id="inpVal" type="search" placeholder="Find Your Favourite Hero or Villain" className="w-[90%] lg:w-[80%] p-2 outline-none rounded-lg focus:ring-4 focus:ring-red-400 focus:bg-blue-100 " onChange={(e)=>{
